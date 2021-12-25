b671396d17f352e286e97e8ebfc47e0b5f666fcb
Tran Ngoc Linh <1851171595@e.tlu.edu.vn>
2021-12-25 08:29:17 +0700

    Trần Ngọc Linh nộp bài tập 1,2,3 Java8

--- BTTB3/src/bttb3/Animal.java
+++ BTTB3/src/bttb3/Animal.java
@@ -9,6 +9,58 @@ package bttb3;
  *
  * @author Asus
  */
-public interface NewInterface {
-    
+public interface Animal {
+    void eat();
+    void breath();
+    default void run(){
+        
+    }
+}    
+    class Dog implements Animal{
+
+    @Override
+    public void run() {
+        Animal.super.run(); //To change body of generated methods, choose Tools | Templates.
+    }     
+
+    @Override
+    public void eat() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void breath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+    class Cat implements Animal{
+
+    @Override
+    public void run() {
+        Animal.super.run(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eat() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void breath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+}
+    class Fish implements Animal{
+
+    @Override
+    public void eat() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void breath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
 }
--- BTTB3/src/bttb3/LambdaExpression.java
+++ BTTB3/src/bttb3/LambdaExpression.java
@@ -9,13 +9,22 @@ package bttb3;
  *
  * @author Asus
  */
-public class NewMain {
-
+public class LambdaExpression implements Movie {
+                Movie mv = () -> {
+		System.out.println("Create movie object with Lambda expression");
+	};
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LambdaExpression le = new LambdaExpression();
+		 le.mv.detail();
+    }
+
+    @Override
+    public void detail() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
